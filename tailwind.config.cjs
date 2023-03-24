50d06398f687bf5dcb884755a045d923430e5333
Hieu Do <hieudn.uh@gmail.com>
2023-03-25 01:20:08 +0700

    style: improve theme color contrast

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'gray': {
+					'50': '#f7f8f9',
+          '100': '#ebedee',
+          '200': '#d1d4d7',
+          '300': '#b5babf',
+          '400': '#7d868f',
+          '500': '#0f1922',
+          '600': '#0d151e',
+          '700': '#0a1018',
+          '800': '#070c11',
+					'900': '#04070a',
+				}
+			}
+		},
 	},
 	darkMode: 'class',
 	plugins: [
