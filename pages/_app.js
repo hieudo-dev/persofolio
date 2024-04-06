180a1c6ac88f8b213c15ca31adab335d6fc09355
Hieu Do <hieudn.uh@gmail.com>
2024-04-06 23:19:30 +0700

    perf: integrate LogRocket

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
+import { useEffect } from "react";
+import LogRocket from "logrocket";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    LogRocket.init("k58rnb/hieudodev");
+  }, []);
+
   return <Component {...pageProps} />;
 }
